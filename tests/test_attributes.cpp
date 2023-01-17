1e8525845fe0301469a2811530b4be213f3b983f
Bailey Chittle <baileyc@tessonics.com>
2023-01-17 10:18:29 -0500

    this works again?

--- tests/test_attributes.cpp
+++ tests/test_attributes.cpp
@@ -31,9 +31,9 @@ TEST_CASE("async attributes test with threads ", "[attributes]")
             // therefore, messages from the same logger object have to be in the same thread
             // to guarantee thread safety, use a different logger object for each thread
             for (int i = 0; i < num_msgs; ++i) {
-                // lg->push_context({{"key"+std::to_string(i), "val"+std::to_string(i)}});
+                lg->push_context({{"key"+std::to_string(i), "val"+std::to_string(i)}});
                 lg->info("testing {}", i);
-                // lg->pop_context();
+                lg->pop_context();
             }
         });
     }
@@ -52,12 +52,10 @@ TEST_CASE("async attributes test with threads ", "[attributes]")
     REQUIRE(overrun_counter == 0);
 
     // todo: better parsing of keys and values, maybe using regex
-    #if 0
     for (auto& line : test_sink->lines()) {
         REQUIRE(line.find("key") != std::string::npos);
         REQUIRE(line.find("val") != std::string::npos);
     }
-    #endif
 }
 
 // testing custom patterns, including the new attribute patterns, with many use cases.
